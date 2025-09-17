e9e6c845caf5c4045920cc494614cc0705821c67
TonyLiu0430 <104262003+TonyLiu0430@users.noreply.github.com>
2025-09-18 00:44:43 +0800

    update Mo's algo

--- Other/Mo_algorithm.cpp
+++ Other/Mo_algorithm.cpp
@@ -1,28 +1,38 @@
+/* nums 長度 N ;; query 長度為 M */
 
-int l = 0, r = 0, nowAns = 0, BLOCK_SIZE, n, m;
-int ans[];
-struct QUE{
+struct Query {
     int l, r, id;
-    friend bool operator < (QUE a, QUE b){
-        if(a.l / BLOCK_SIZE != b.l / BLOCK_SIZE)
-            return a.l / BLOCK_SIZE < b.l / BLOCK_SIZE;
-        return a.r < b.r;
-    }
-}querys[];
+};
 
-inline void move(int pos, int sign) {
-    // update nowAns
+void add(int pos) {
+    /*更新狀態*/
+    /*將pos所在的移入集合*/
 }
 
-void solve() {
-    BLOCK_SIZE = int(ceil(pow(n, 0.5)));
-    sort(querys, querys + m);
-    for (int i = 0; i < m; ++i) {
-        const QUE &q = querys[i];
-        while (l > q.l) move(--l, 1);
-        while (r < q.r) move(r++, 1);
-        while (l < q.l) move(l++, -1);
-        while (r > q.r) move(--r, -1);
-        ans[q.id] = nowAns;
+void del(int pos) {
+    /*更新狀態*/
+    /*將pos所在的移出集合*/
+}
+
+int bsz = n / sqrt(m); /*分塊大小 block size*/
+sort(query.begin(), query.end(), [bsz](const Query &a, const Query &b){
+    if(a.l / bsz != b.l / bsz) {
+        return a.l < b.l;
     }
+    return (a.l / bsz) & 1 ? a.r < b.r : a.r > b.r;
+});
+
+int l = 1;
+int r = 0;
+
+vector<pair<int, int>> res(m);
+
+for(int i = 0; i < query.size(); i++ ) {
+    auto &q = query[i];
+    /*順序不能換*/
+    while (l > q.l) add(--l);
+    while (r < q.r) add(++r);
+    while (l < q.l) del(l++);
+    while (r > q.r) del(r--);
+    res[q.id] = /* 根據當前狀態求解 */
 }
