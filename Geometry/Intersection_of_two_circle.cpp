56192dca69414fbce0a00a97fc5cad7f3612d17c
TonyLiu0430 <104262003+TonyLiu0430@users.noreply.github.com>
2025-09-23 18:44:16 +0800

    update

--- Geometry/Intersection_of_two_circle.cpp
+++ Geometry/Intersection_of_two_circle.cpp
@@ -1,9 +1,9 @@
-vector<Double> interCircle(Double o1, Double r1, Double o2, Double r2) {
-	Double d2 = abs2(o1 - o2);
-	Double d = sqrt(d2);
-	if (d < fabs(r1-r2) || r1+r2 < d) return {};
-	Double u = 0.5*(o1+o2) + ((r2*r2-r1*r1)/(2.0*d2))*(o1-o2);
-	Double A = sqrt((r1+r2+d) * (r1-r2+d) * (r1+r2-d) * (-r1+r2+d));
-	Double v = A / (2.0*d2) * Double(o1.S-o2.S, -o1.F+o2.F);
-	return {u+v, u-v};
+vector<Point> interCircle(Point o1, type r1, Point o2, type r2) {
+    type d2 = abs2(o1 - o2);
+    type d = sqrt(d2);
+    if (d < fabs(r1 - r2) || d > r1 + r2) return {};
+    Point u = (o1 + o2) * 0.5 + ((r2*r2 - r1*r1) / (2.0*d2)) * (o1 - o2);
+    type A = sqrt((r1+r2+d) * (r1-r2+d) * (r1+r2-d) * (-r1+r2+d));
+    Point v = Point{o1.y - o2.y, -(o1.x - o2.x)} * (A / (2.0*d2));
+    return { u + v, u - v };
 }
