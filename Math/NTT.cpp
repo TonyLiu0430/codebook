0a2580b312345a11d1e09f89f352289bbc971b3c
TonyLiu0430 <104262003+TonyLiu0430@users.noreply.github.com>
2025-10-10 15:28:14 +0800

    update

--- Math/NTT.cpp
+++ Math/NTT.cpp
@@ -1,37 +1,41 @@
-/*預處理 lim lim >= lenA + lenB - 1*/
-while (lim < (lenA + lenB - 1)) lim <<= 1;
-/*預處理 r*/
-int r[N];
-for (i = 0; i < lim; ++i) r[i] = (i & 1) * (lim >> 1) + (r[i >> 1] >> 1);
-///////////////////////////////////////
 constexpr int P = 998244353; 
 const int G = 3;
-/*998244353	3
-1004535809	3
-469762049	3
-167772161	3
-754974721	11*/
-// lim 一定要是 2 的冪次   opt == -1 => inverse ntt
-// x.size() == lim
-void ntt(vector<int> &x, int lim, int opt) { 
-  int i, j, k, m, gn, g, tmp;
-  for (i = 0; i < lim; ++i)
-    if (r[i] < i) swap(x[i], x[r[i]]);
-  for (m = 2; m <= lim; m <<= 1) {
-    k = m >> 1;
-    gn = qpow(G, (P - 1) / m);
-    for (i = 0; i < lim; i += m) {
-      g = 1;
-      for (j = 0; j < k; ++j, g = 1ll * g * gn % P) {
-        tmp = 1ll * x[i + j + k] * g % P;
-        x[i + j + k] = (x[i + j] - tmp + P) % P;
-        x[i + j] = (x[i + j] + tmp) % P;
-      }
+/*預處理 lim*/
+int lim = 1;
+while (lim < (lenSum - 1)) lim <<= 1;
+/*每個多項式都要resize(lim)*/
+/*998244353	3 1004535809 3 469762049 3 167772161 3 754974721 11*/
+void init_rev(vector<int> &rev, int lim) {
+    int lg = __builtin_ctz(lim); // lim 是 2^k
+    rev.resize(lim);
+    for (int i = 0; i < lim; ++i)
+        rev[i] = (rev[i >> 1] >> 1) | ((i & 1) << (lg - 1));
+}
+// a.size() == lim
+void ntt(vector<int> &a, int opt) { // opt == -1 => reverse ntt
+    int n = a.size();
+    static vector<int> rev;
+    init_rev(rev, n);
+    for (int i = 0; i < n; ++i)
+        if (i < rev[i]) swap(a[i], a[rev[i]]);
+    
+    for (int m = 2; m <= n; m <<= 1) {
+        int k = m >> 1;
+        int gn = qpow(G, (P - 1) / m);
+        if (opt == -1) gn = qpow(gn, P - 2);
+        for (int i = 0; i < n; i += m) {
+            int g = 1;
+            for (int j = 0; j < k; ++j) {
+                int t = 1ll * a[i + j + k] * g % P;
+                a[i + j + k] = (a[i + j] - t + P) % P;
+                a[i + j] = (a[i + j] + t) % P;
+                g = 1ll * g * gn % P;
+            }
+        }
     }
-  }
-  if (opt == -1) {
-    reverse(x + 1, x + lim);
-    int inv = qpow(lim, P - 2);
-    for (i = 0; i < lim; ++i) x[i] = 1ll * x[i] * inv % P;
-  }
+
+    if (opt == -1) {
+        int inv_n = qpow(n, P - 2);
+        for (int &x : a) x = 1ll * x * inv_n % P;
+    }
 }
