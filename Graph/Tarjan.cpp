86c7a642848dbcfd1c8736d5661dbb0f085e8dae
TonyLiu0430 <tony04302020@gmail.com>
2025-11-14 21:23:54 +0800

    update tarjan

--- Graph/Tarjan.cpp
+++ Graph/Tarjan.cpp
@@ -1,3 +1,52 @@
+struct TarjanSCC {
+	// n must be first
+    int n, timer = 0, scc_cnt = 0;
+    vector<vector<int>> g;
+    vector<int> dfn, low, in_stack, scc_id;
+    stack<int> stk;
+
+    TarjanSCC(int n) : n(n), g(n), dfn(n, -1), low(n), in_stack(n, 0), scc_id(n, -1) {}
+
+    void add_edge(int u, int v) {
+        g[u].push_back(v);
+    }
+
+    void dfs(int u) {
+        dfn[u] = low[u] = timer++;
+        stk.push(u);
+        in_stack[u] = 1;
+
+        for (int v : g[u]) {
+            if (dfn[v] == -1) {
+                dfs(v);
+                low[u] = min(low[u], low[v]);
+            } else if (in_stack[v]) {
+                low[u] = min(low[u], dfn[v]);
+            }
+        }
+
+        // root of SCC
+        if (low[u] == dfn[u]) {
+            while (true) {
+                int v = stk.top(); stk.pop();
+                in_stack[v] = 0;
+                scc_id[v] = scc_cnt;
+                if (v == u) break;
+            }
+            scc_cnt++;
+        }
+    }
+
+    // returns SCC count
+    int run() {
+        for (int i = 0; i < n; i++)
+            if (dfn[i] == -1)
+                dfs(i);
+        return scc_cnt;
+    }
+};
+
+// tarjan BCC
 割點
 點 u 為割點 if and only if 滿足 1. or 2. 
 1. u 爲樹根，且 u 有多於一個子樹。 
@@ -6,50 +55,22 @@
 -------------------------------------------------------
 橋
 一條無向邊 (u,v) 是橋 if and only if (u,v) 爲樹枝邊，且滿足 DFN(u) < Low(v)。
+int n, timer = 0;
+vector<vector<int>> g;
+vector<int> dfn /* init by -1*/, low;
 
-// 0 base
-struct TarjanSCC{
-	static const int MAXN = 1000006;
-	int n, dfn[MAXN], low[MAXN], scc[MAXN], scn, count;
-	vector<int> G[MAXN];
-	stack<int> stk;
-	bool ins[MAXN];
+void dfs(int u, int parent) {
+    dfn[u] = low[u] = timer++;
 
-	void tarjan(int u){
-		dfn[u] = low[u] = ++count;
-		stk.push(u);
-		ins[u] = true;
+    for (int v : g[u]) {
+        if (v == parent) continue;
 
-		for(auto v:G[u]){
-			if(!dfn[v]){
-				tarjan(v);
-				low[u] = min(low[u], low[v]);
-			}else if(ins[v]){
-				low[u] = min(low[u], dfn[v]);
-			}
-		}
+        if (dfn[v] == -1) {
+            dfs(v, u);
+            low[u] = min(low[u], low[v]);
+        } else {
+            low[u] = min(low[u], dfn[v]);
+        }
+    }
+}
 
-		if(dfn[u] == low[u]){
-			int v;
-			do {
-			v = stk.top();
-			stk.pop();
-			scc[v] = scn;
-			ins[v] = false;
-			} while(v != u);
-			scn++;
-		}
-	}
-
-	void getSCC(){
-		memset(dfn,0,sizeof(dfn));
-		memset(low,0,sizeof(low));
-		memset(ins,0,sizeof(ins));
-		memset(scc,0,sizeof(scc));
-		count = scn = 0;
-		for(int i = 0 ; i < n ; i++ ){
-			if(!dfn[i]) tarjan(i);
-		}
-	}
-
-}SCC;
